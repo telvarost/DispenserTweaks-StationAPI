ead20b9330e8c0ce3fcdb8cd2cb3cd299ec382aa
telontelvarost <telontelvarost@gmail.com>
2024-05-05 18:51:08 -0400

    UDOC-0 - Code up most of dispenser behavior; Still missing configs; Small bug exists with dispensing water next to lava

--- src/main/java/com/github/telvarost/dispensertweaks/events/DispenserListener.java
+++ src/main/java/com/github/telvarost/dispensertweaks/events/DispenserListener.java
@@ -1,5 +1,6 @@
 package com.github.telvarost.dispensertweaks.events;
 
+import com.github.telvarost.dispensertweaks.ModHelper;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -17,96 +18,140 @@ public class DispenserListener {
         World world = context.dispenser.world;
 
         if (context.itemStack != null) {
-//            // Make arrows drop as an item instead of shoot as an entity
-//            if (context.itemStack.itemId == Item.ARROW.id) {
-//                context.skipSpecial = true;
-//            }
-
             BlockPos facing = context.getFacingBlockPos();
 
             // Make buckets pickup liquids
             if (context.itemStack.itemId == Item.BUCKET.id) {
                 if (world.method_1779(facing.x, facing.y, facing.z) == Material.WATER && world.getBlockMeta(facing.x, facing.y, facing.z) == 0) {
-                    world.setBlock(facing.x, facing.y, facing.z, 0);
+                    world.method_154(facing.x, facing.y, facing.z, 0, 0);
+                    world.method_244(facing.x, facing.y, facing.z, Block.WATER.id);
                     context.dispenser.setStack(context.slot, new ItemStack(Item.WATER_BUCKET));
+                    world.method_230(1000, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
                     event.cancel();
                 }
 
                 if (world.method_1779(facing.x, facing.y, facing.z) == Material.LAVA && world.getBlockMeta(facing.x, facing.y, facing.z) == 0) {
-                    world.setBlock(facing.x, facing.y, facing.z, 0);
+                    world.method_154(facing.x, facing.y, facing.z, 0, 0);
+                    world.method_244(facing.x, facing.y, facing.z, Block.LAVA.id);
                     context.dispenser.setStack(context.slot, new ItemStack(Item.LAVA_BUCKET));
+                    world.method_230(1000, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
                     event.cancel();
                 }
             }
 
             // Make water buckets place water
             if (context.itemStack.itemId == Item.WATER_BUCKET.id) {
-                if (world.method_1779(facing.x, facing.y, facing.z) == Material.AIR) {
-                    world.setBlockStateWithNotify(facing.x, facing.y, facing.z, Block.WATER.getDefaultState());
-                    world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.WATER.id);
-                    context.dispenser.setStack(context.slot, new ItemStack(Item.BUCKET));
-                    event.cancel();
+                if (world.method_1779(facing.x, facing.y, facing.z) == Material.AIR || !world.method_1779(facing.x, facing.y, facing.z).method_905()) {
+                    if (world.method_1779(facing.x, facing.y, facing.z) != Material.AIR) {
+                        Block.BLOCKS[world.getBlockId(facing.x, facing.y, facing.z)].dropStacks(world, facing.x, facing.y, facing.z, world.getBlockMeta(facing.x, facing.y, facing.z));
+                        world.method_154(facing.x, facing.y, facing.z, Block.WATER.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.WATER.id);
+                        context.dispenser.setStack(context.slot, new ItemStack(Item.BUCKET));
+                        world.playSound(context.dispenser.x, context.dispenser.y, context.dispenser.z, "liquid.splash", 0.5F, 2.6F + (world.field_214.nextFloat() - world.field_214.nextFloat()) * 0.8F);
+                        event.cancel();
+                    } else {
+                        world.method_154(facing.x, facing.y, facing.z, Block.WATER.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.WATER.id);
+                        context.dispenser.setStack(context.slot, new ItemStack(Item.BUCKET));
+                        world.playSound(context.dispenser.x, context.dispenser.y, context.dispenser.z, "liquid.splash", 0.5F, 2.6F + (world.field_214.nextFloat() - world.field_214.nextFloat()) * 0.8F);
+                        event.cancel();
+                    }
                 }
             }
 
             // Make lava buckets place lava
             if (context.itemStack.itemId == Item.LAVA_BUCKET.id) {
-                if (world.method_1779(facing.x, facing.y, facing.z) == Material.AIR) {
-                    world.setBlockStateWithNotify(facing.x, facing.y, facing.z, Block.LAVA.getDefaultState());
-                    world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.LAVA.id);
-                    context.dispenser.setStack(context.slot, new ItemStack(Item.BUCKET));
+                if (world.method_1779(facing.x, facing.y, facing.z) == Material.AIR || !world.method_1779(facing.x, facing.y, facing.z).method_905()) {
+                    if (world.method_1779(facing.x, facing.y, facing.z) != Material.AIR) {
+                        Block.BLOCKS[world.getBlockId(facing.x, facing.y, facing.z)].dropStacks(world, facing.x, facing.y, facing.z, world.getBlockMeta(facing.x, facing.y, facing.z));
+                        world.method_154(facing.x, facing.y, facing.z, Block.LAVA.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.LAVA.id);
+                        context.dispenser.setStack(context.slot, new ItemStack(Item.BUCKET));
+                        world.method_230(1002, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
+                        event.cancel();
+                    } else {
+                        world.method_154(facing.x, facing.y, facing.z, Block.LAVA.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.LAVA.id);
+                        context.dispenser.setStack(context.slot, new ItemStack(Item.BUCKET));
+                        world.method_230(1002, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
+                        event.cancel();
+                    }
+                }
+            }
+
+            // Special Modded Place/Pick Up Water Tiles
+            if (context.itemStack.itemId == Block.FLOWING_WATER.id) {
+                if (world.method_1779(facing.x, facing.y, facing.z) == Material.AIR || !world.method_1779(facing.x, facing.y, facing.z).method_905()) {
+                    if (  (-1 < ModHelper.ModHelperFields.emptySlotAvailable)
+                       && (0 == world.getBlockMeta(facing.x, facing.y, facing.z))
+                       && (  (Block.FLOWING_WATER.id == world.getBlockId(facing.x, facing.y, facing.z))
+                          || (Block.WATER.id == world.getBlockId(facing.x, facing.y, facing.z))
+                          )
+                    ) {
+                        world.method_154(facing.x, facing.y, facing.z, 0, 0);
+                        world.method_244(facing.x, facing.y, facing.z, Block.WATER.id);
+                        context.dispenser.setStack(context.slot, new ItemStack(Block.FLOWING_WATER));
+                        context.dispenser.setStack(ModHelper.ModHelperFields.emptySlotAvailable, new ItemStack(Block.FLOWING_WATER));
+                        world.method_230(1000, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
+                        event.cancel();
+                    } else if (world.method_1779(facing.x, facing.y, facing.z) != Material.AIR) {
+                        Block.BLOCKS[world.getBlockId(facing.x, facing.y, facing.z)].dropStacks(world, facing.x, facing.y, facing.z, world.getBlockMeta(facing.x, facing.y, facing.z));
+                        world.method_154(facing.x, facing.y, facing.z, Block.WATER.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.WATER.id);
+                        world.playSound(context.dispenser.x, context.dispenser.y, context.dispenser.z, "liquid.splash", 0.5F, 2.6F + (world.field_214.nextFloat() - world.field_214.nextFloat()) * 0.8F);
+                        event.cancel();
+                    } else {
+                        world.method_154(facing.x, facing.y, facing.z, Block.WATER.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.WATER.id);
+                        world.playSound(context.dispenser.x, context.dispenser.y, context.dispenser.z, "liquid.splash", 0.5F, 2.6F + (world.field_214.nextFloat() - world.field_214.nextFloat()) * 0.8F);
+                        event.cancel();
+                    }
+                } else {
+                    context.dispenser.setStack(context.slot, new ItemStack(Block.FLOWING_WATER.asItem()));
+                    world.playSound(context.dispenser.x, context.dispenser.y, context.dispenser.z, "random.fizz", 0.5F, 2.6F + (world.field_214.nextFloat() - world.field_214.nextFloat()) * 0.8F);
+                    for (int var28 = 0; var28 < 8; ++var28) {
+                        world.addParticle("largesmoke", (double) context.dispenser.x + Math.random(), (double) context.dispenser.y + Math.random(), (double) context.dispenser.z + Math.random(), 0.0, 0.0, 0.0);
+                    }
                     event.cancel();
                 }
             }
 
-            // Special L
-            // else if (Config.ConfigFields.moddedDispenserFluidPlacement && var12.itemId == BlockBase.FLOWING_WATER.id) {
-            //                if (0 == blockIdInFrontOfDispenser || !arg.getMaterial(i + x_axis, j, k + z_axis).isSolid()) {
-            //                    if (  (-1 < ModHelper.ModHelperFields.emptySlotAvailable)
-            //                       && (0 == arg.getTileMeta(i + x_axis, j, k + z_axis))
-            //                       && ( (BlockBase.FLOWING_WATER.id == blockIdInFrontOfDispenser)
-            //                          || (BlockBase.STILL_WATER.id == blockIdInFrontOfDispenser)
-            //                          )
-            //                    ) {
-            //                        arg.placeBlockWithMetaData(i + x_axis, j, k + z_axis, 0, 0);
-            //                        arg.playLevelEvent(1000, i, j, k, 0);
-            //                        var11.setInventoryItem(ModHelper.ModHelperFields.lastSlotDispensed, new ItemInstance(BlockBase.FLOWING_WATER, 1));
-            //                        var11.setInventoryItem(ModHelper.ModHelperFields.emptySlotAvailable, new ItemInstance(BlockBase.FLOWING_WATER, 1));
-            //                    } else {
-            //                        arg.placeBlockWithMetaData(i + x_axis, j, k + z_axis, FLOWING_WATER.id, 0);
-            //                        arg.playSound(i, j, k, "liquid.splash", 0.5F, 2.6F + (arg.rand.nextFloat() - arg.rand.nextFloat()) * 0.8F);
-            //                    }
-            //                } else {
-            //                    var11.setInventoryItem(ModHelper.ModHelperFields.lastSlotDispensed, new ItemInstance(BlockBase.FLOWING_WATER, 1));
-            //                    arg.playSound(i, j, k, "random.fizz", 0.5F, 2.6F + (arg.rand.nextFloat() - arg.rand.nextFloat()) * 0.8F);
-            //                    for (int var28 = 0; var28 < 8; ++var28) {
-            //                        arg.addParticle("largesmoke", (double) i + Math.random(), (double) j + Math.random(), (double) k + Math.random(), 0.0, 0.0, 0.0);
-            //                    }
-            //                }
-            //            } else if (Config.ConfigFields.moddedDispenserFluidPlacement && var12.itemId == BlockBase.FLOWING_LAVA.id) {
-            //                if (0 == blockIdInFrontOfDispenser || !arg.getMaterial(i + x_axis, j, k + z_axis).isSolid()) {
-            //                    if (  (-1 < ModHelper.ModHelperFields.emptySlotAvailable)
-            //                       && (0 == arg.getTileMeta(i + x_axis, j, k + z_axis))
-            //                       && (  (BlockBase.FLOWING_LAVA.id == blockIdInFrontOfDispenser)
-            //                          || (BlockBase.STILL_LAVA.id == blockIdInFrontOfDispenser)
-            //                          )
-            //                    ) {
-            //                        arg.placeBlockWithMetaData(i + x_axis, j, k + z_axis, 0, 0);
-            //                        arg.playLevelEvent(1000, i, j, k, 0);
-            //                        var11.setInventoryItem(ModHelper.ModHelperFields.lastSlotDispensed, new ItemInstance(BlockBase.FLOWING_LAVA, 1));
-            //                        var11.setInventoryItem(ModHelper.ModHelperFields.emptySlotAvailable, new ItemInstance(BlockBase.FLOWING_LAVA, 1));
-            //                    } else {
-            //                        arg.placeBlockWithMetaData(i + x_axis, j, k + z_axis, FLOWING_LAVA.id, 0);
-            //                        arg.playLevelEvent(1002, i, j, k, 0);
-            //                    }
-            //                } else {
-            //                    var11.setInventoryItem(ModHelper.ModHelperFields.lastSlotDispensed, new ItemInstance(BlockBase.FLOWING_LAVA, 1));
-            //                    arg.playSound(i + 0.5, j + 0.5, k + 0.5, "random.fizz", 0.5F, 2.6F + (arg.rand.nextFloat() - arg.rand.nextFloat()) * 0.8F);
-            //                    for (int var28 = 0; var28 < 8; ++var28) {
-            //                        arg.addParticle("largesmoke", (double) i + Math.random(), (double) j + Math.random(), (double) k + Math.random(), 0.0, 0.0, 0.0);
-            //                    }
-            //                }
-            //            }
+            // Special Modded Place/Pick Up Lava Tiles
+            if (context.itemStack.itemId == Block.FLOWING_LAVA.id) {
+                if (world.method_1779(facing.x, facing.y, facing.z) == Material.AIR || !world.method_1779(facing.x, facing.y, facing.z).method_905()) {
+                    if (  (-1 < ModHelper.ModHelperFields.emptySlotAvailable)
+                       && (0 == world.getBlockMeta(facing.x, facing.y, facing.z))
+                       && (  (Block.FLOWING_LAVA.id == world.getBlockId(facing.x, facing.y, facing.z))
+                          || (Block.LAVA.id == world.getBlockId(facing.x, facing.y, facing.z))
+                       )
+                    ) {
+                        world.method_154(facing.x, facing.y, facing.z, 0, 0);
+                        world.method_244(facing.x, facing.y, facing.z, Block.LAVA.id);
+                        context.dispenser.setStack(context.slot, new ItemStack(Block.FLOWING_LAVA));
+                        context.dispenser.setStack(ModHelper.ModHelperFields.emptySlotAvailable, new ItemStack(Block.FLOWING_LAVA));
+                        world.method_230(1000, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
+                        event.cancel();
+                    } else if (world.method_1779(facing.x, facing.y, facing.z) != Material.AIR) {
+                        Block.BLOCKS[world.getBlockId(facing.x, facing.y, facing.z)].dropStacks(world, facing.x, facing.y, facing.z, world.getBlockMeta(facing.x, facing.y, facing.z));
+                        world.method_154(facing.x, facing.y, facing.z, Block.LAVA.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.LAVA.id);
+                        world.method_230(1002, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
+                        event.cancel();
+                    } else {
+                        world.method_154(facing.x, facing.y, facing.z, Block.LAVA.id, 0);
+                        world.method_244(context.dispenser.x, context.dispenser.y, context.dispenser.z, Block.LAVA.id);
+                        world.method_230(1002, context.dispenser.x, context.dispenser.y, context.dispenser.z, 0);
+                        event.cancel();
+                    }
+                } else {
+                    context.dispenser.setStack(context.slot, new ItemStack(Block.FLOWING_WATER.asItem()));
+                    world.playSound(context.dispenser.x, context.dispenser.y, context.dispenser.z, "random.fizz", 0.5F, 2.6F + (world.field_214.nextFloat() - world.field_214.nextFloat()) * 0.8F);
+                    for (int var28 = 0; var28 < 8; ++var28) {
+                        world.addParticle("largesmoke", (double) context.dispenser.x + Math.random(), (double) context.dispenser.y + Math.random(), (double) context.dispenser.z + Math.random(), 0.0, 0.0, 0.0);
+                    }
+                    event.cancel();
+                }
+            }
         }
     }
 }
